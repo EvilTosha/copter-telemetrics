28fe3397d654d2feb374200866c4847bf4e77991
EvilTosha <toshaevil@gmail.com>
2013-03-19 21:50:17 +0400

    new parsing of input && motors power display

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,8 +10,6 @@ MainWindow::MainWindow(QWidget *parent) :
 	m_tcpSocket = new QTcpSocket(this);
 	connect(m_tcpSocket, SIGNAL(readyRead()), this, SLOT(onTcpRead()));
 
-	ui->logTextBrowser->setText("ololo");
-	ui->logTextBrowser->update();
 	connect(ui->connectButton, SIGNAL(clicked()), this, SLOT(connectToServer()));
 	connect(ui->disconnectButton, SIGNAL(clicked()), this, SLOT(disconnectToServer()));
 }
@@ -19,6 +17,7 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
 	delete ui;
+	m_tcpSocket->close();
 }
 
 void MainWindow::onTcpRead()
@@ -29,8 +28,20 @@ void MainWindow::onTcpRead()
 		char data[1024];
 		m_tcpSocket->readLine(data, 1024);
 		QString line(data);
-		if (!line.isEmpty())
+		if (!line.isEmpty()) {
 			ui->logTextBrowser->setText(line);
+			QStringList values = line.split(",");
+			if (values.length() < 11) {
+				qDebug() << "Wrong format of debug input";
+			}
+			else {
+				ui->powerAllLcd->display(values.at(6).toInt());
+				ui->powerX1Lcd->display(values.at(7).toInt());
+				ui->powerX2Lcd->display(values.at(8).toInt());
+				ui->powerY1Lcd->display(values.at(9).toInt());
+				ui->powerY2Lcd->display(values.at(10).toInt());
+			}
+		}
 		ui->logTextBrowser->update();
 	}
 }
